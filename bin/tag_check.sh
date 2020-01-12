8ff33378324be9904527c11dd50f42648a9b8f23
Edward Rudd <urkle@outoforder.cc>
2020-01-12 13:48:14 -0500

    upgrade to circle 2.0

---
+++ bin/tag_check.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+tag=`git describe --tags --exact-match  HEAD 2> /dev/null`
+
+if [ $? -eq 0 ]; then
+  version=`grep VERSION lib/sidekiq-instrumental/version.rb | sed -e "s/.*'\([^']*\)'.*/\1/"`
+  
+  if [ "v$version" = "$tag" ]; then
+    echo "Revision $tag Matches $version"
+  else
+    echo "Revision $tag does not match $version"
+    exit 2
+  fi
+else
+  echo "No tag found"
+  exit 1
+fi
